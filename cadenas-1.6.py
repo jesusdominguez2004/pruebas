e05db13c59efdd5bb73b8e1eb2516f92f12cb204
jesusdominguez2004 <103864058+jesusdominguez2004@users.noreply.github.com>
2022-11-30 11:18:29 -0500

    Add files via upload

---
+++ cadenas-1.6.py
@@ -0,0 +1,263 @@
+
+# Cadenas 1.6 | Jesús Alberto Domínguez Charris
+
+"""
+Secuencial
+cadena:
+    invertida
+    longitud
+    vector
+    palabras
+
+    vocales SIN REPETIR + TILDES
+    consonantes SIN REPETIR
+    números SIN REPETIR
+    signos de puntuación SIN REPETIR
+    otros caracteres SIN REPETIR
+
+    palabras CANTIDAD
+    vocales CANTIDAD
+    consonantes CANTIDAD
+    números CANTIDAD
+    signos de puntuación CANTIDAD
+    otros caracteres CANTIDAD
+    mayúsculas CANTIDAD
+    minúsculas CANTIDAD
+    espacios CANTIDAD
+
+menú
+guardar en txt
+leer txt
+"""
+
+while True:
+    print("CADENAS 1.6")
+    print("1. Consola\n2. Guardar en txt\n3. Leer txt\n4. Salir")
+    menu = input(">>> ")
+
+    if menu == "1":
+        cadena = input("Ingrese su cadena: ")
+        # 1. Invertir
+        invertir = cadena[::-1]
+        # 2. Longitud
+        longitud = len(cadena)
+        # 3. Vector caracteres
+        vector = []
+        for x in cadena:
+            vector.append(x)
+        # 4. Lista palabras
+        palabras = cadena.split()
+        cantidad_palabras = len(palabras)
+        # 5. Vocales
+        cantidad_vocales = 0
+        vocales = ["a", "e", "i", "o", "u",
+                   "á", "é", "í", "ó", "ú",
+                   "à", "è", "ì", "ò", "ù",
+                   "ä", "ë", "ï", "ö", "ü"]
+        vocales_cadena = []
+        for x in cadena:
+            if x.lower() in vocales:
+                cantidad_vocales += 1
+                if x not in vocales_cadena:
+                    vocales_cadena.append(x)
+        # 6. Consonantes
+        cantidad_consonantes = 0
+        consonantes = ["b", "c", "d", "f",
+                       "g", "h", "j", "k",
+                       "l", "m", "n", "ñ",
+                       "p", "q", "r", "s",
+                       "t", "v", "w", "x",
+                       "y", "z"]
+        consonantes_cadena = []
+        for x in cadena:
+            if x.lower() in consonantes:
+                cantidad_consonantes += 1
+                if x not in consonantes_cadena:
+                    consonantes_cadena.append(x)
+        # 7. Números
+        cantidad_numeros = 0
+        numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        numeros_cadena = []
+        for x in cadena:
+            if x in numeros:
+                cantidad_numeros += 1
+                if x not in numeros_cadena:
+                    numeros_cadena.append(x)
+        # 8. Signos de puntuación
+        cantidad_puntuacion = 0
+        puntuacion = ["¿", "?", "¡", "!",
+                      "(", ")", "[", "]",
+                      "{", "}", ".", ":",
+                      ",", ";"]
+        puntuacion_cadena = []
+        for x in cadena:
+            if x.lower() in puntuacion:
+                cantidad_puntuacion += 1
+                if x not in puntuacion_cadena:
+                    puntuacion_cadena.append(x)
+        # 9. Otros caracteres
+        cantidad_otros = 0
+        otros = ["+", "-", "*", "/", "%",
+                 "<", ">", "=", "|", "°",
+                 "¬", "#", "$", "&", "\\",
+                 "¨", "´", "~", "^", "`",
+                 "_", "@"]
+        otros_cadena = []
+        for x in cadena:
+            if x.lower() in otros:
+                cantidad_otros += 1
+                if x not in otros_cadena:
+                    otros_cadena.append(x)
+        # 10. Cantidad mayúsculas y minúsculas
+        cantidad_mayusculas = 0
+        cantidad_minusculas = 0
+        for x in cadena:
+            if x.isupper():
+                cantidad_mayusculas += 1
+            if x.islower():
+                cantidad_minusculas += 1
+        # 11. Cantidad espacios
+        cantidad_espacios = 0
+        for x in cadena:
+            if x.isspace():
+                cantidad_espacios += 1
+        # Imprimir resultados
+        print(f"Cadena: {cadena}")
+        print(f"1. Invertir: {invertir}")
+        print(f"2. Longitud: {longitud}")
+        print(f"3. Lista caracteres: {vector}")
+        print(f"4. Lista palabras: {palabras}")
+        print(f"5. Lista vocales: {vocales_cadena}")
+        print(f"6. Lista consonantes: {consonantes_cadena}")
+        print(f"7. Lista números: {numeros_cadena}")
+        print(f"8. Lista signos de puntuación: {puntuacion_cadena}")
+        print(f"9. Lista otros caracteres: {otros_cadena}")
+        print(f"10. Cantidad palabras: {cantidad_palabras}")
+        print(f"11. Cantidad vocales: {cantidad_vocales}")
+        print(f"12. Cantidad consonantes: {cantidad_consonantes}")
+        print(f"13. Cantidad números: {cantidad_numeros}")
+        print(f"14. Cantidad signos de puntuación: {cantidad_puntuacion}")
+        print(f"15. Cantidad otros caracteres: {cantidad_otros}")
+        print(f"16. Cantidad mayúsculas: {cantidad_mayusculas}")
+        print(f"17. Cantidad minúsculas: {cantidad_minusculas}")
+        print(f"18. Cantidad espacios: {cantidad_espacios}\n")
+
+    elif menu == "2":
+        cadena = input("Ingrese su cadena: ")
+        # 1. Invertir
+        invertir = cadena[::-1]
+        # 2. Longitud
+        longitud = len(cadena)
+        # 3. Vector caracteres
+        vector = []
+        for x in cadena:
+            vector.append(x)
+        # 4. Lista palabras
+        palabras = cadena.split()
+        cantidad_palabras = len(palabras)
+        # 5. Vocales
+        cantidad_vocales = 0
+        vocales = ["a", "e", "i", "o", "u",
+                   "á", "é", "í", "ó", "ú",
+                   "à", "è", "ì", "ò", "ù",
+                   "ä", "ë", "ï", "ö", "ü"]
+        vocales_cadena = []
+        for x in cadena:
+            if x.lower() in vocales:
+                cantidad_vocales += 1
+                if x not in vocales_cadena:
+                    vocales_cadena.append(x)
+        # 6. Consonantes
+        cantidad_consonantes = 0
+        consonantes = ["b", "c", "d", "f",
+                       "g", "h", "j", "k",
+                       "l", "m", "n", "ñ",
+                       "p", "q", "r", "s",
+                       "t", "v", "w", "x",
+                       "y", "z"]
+        consonantes_cadena = []
+        for x in cadena:
+            if x.lower() in consonantes:
+                cantidad_consonantes += 1
+                if x not in consonantes_cadena:
+                    consonantes_cadena.append(x)
+        # 7. Números
+        cantidad_numeros = 0
+        numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        numeros_cadena = []
+        for x in cadena:
+            if x in numeros:
+                cantidad_numeros += 1
+                if x not in numeros_cadena:
+                    numeros_cadena.append(x)
+        # 8. Signos de puntuación
+        cantidad_puntuacion = 0
+        puntuacion = ["¿", "?", "¡", "!",
+                      "(", ")", "[", "]",
+                      "{", "}", ".", ":",
+                      ",", ";"]
+        puntuacion_cadena = []
+        for x in cadena:
+            if x.lower() in puntuacion:
+                cantidad_puntuacion += 1
+                if x not in puntuacion_cadena:
+                    puntuacion_cadena.append(x)
+        # 9. Otros caracteres
+        cantidad_otros = 0
+        otros = ["+", "-", "*", "/", "%",
+                 "<", ">", "=", "|", "°",
+                 "¬", "#", "$", "&", "\\",
+                 "¨", "´", "~", "^", "`",
+                 "_", "@"]
+        otros_cadena = []
+        for x in cadena:
+            if x.lower() in otros:
+                cantidad_otros += 1
+                if x not in otros_cadena:
+                    otros_cadena.append(x)
+        # 10. Cantidad mayúsculas y minúsculas
+        cantidad_mayusculas = 0
+        cantidad_minusculas = 0
+        for x in cadena:
+            if x.isupper():
+                cantidad_mayusculas += 1
+            if x.islower():
+                cantidad_minusculas += 1
+        # 11. Cantidad espacios
+        cantidad_espacios = 0
+        for x in cadena:
+            if x.isspace():
+                cantidad_espacios += 1
+        # Guardar en un txt
+        with open("cadenas-1.6.txt", "w", encoding="utf-8") as file:
+            file.write(f"Cadena: {cadena}\n")
+            file.write(f"1. Invertir: {invertir}\n")
+            file.write(f"2. Longitud: {longitud}\n")
+            file.write(f"3. Lista caracteres: {vector}\n")
+            file.write(f"4. Lista palabras: {palabras}\n")
+            file.write(f"5. Lista vocales: {vocales_cadena}\n")
+            file.write(f"6. Lista consonantes: {consonantes_cadena}\n")
+            file.write(f"7. Lista números: {numeros_cadena}\n")
+            file.write(f"8. Lista signos de puntuación: {puntuacion_cadena}\n")
+            file.write(f"9. Lista otros caracteres: {otros_cadena}\n")
+            file.write(f"10. Cantidad palabras: {cantidad_palabras}\n")
+            file.write(f"11. Cantidad vocales: {cantidad_vocales}\n")
+            file.write(f"12. Cantidad consonantes: {cantidad_consonantes}\n")
+            file.write(f"13. Cantidad números: {cantidad_numeros}\n")
+            file.write(f"14. Cantidad signos de puntuación: {cantidad_puntuacion}\n")
+            file.write(f"15. Cantidad otros caracteres: {cantidad_otros}\n")
+            file.write(f"16. Cantidad mayúsculas: {cantidad_mayusculas}\n")
+            file.write(f"17. Cantidad minúsculas: {cantidad_minusculas}\n")
+            file.write(f"18. Cantidad espacios: {cantidad_espacios}")
+            print("¡Proceso de guardado finalizado!\n")
+
+    elif menu == "3":
+        # Leer un txt
+        with open("cadenas-1.6.txt", "r", encoding="utf-8") as file:
+            contenido = file.read()
+            print(contenido + "\n")
+            print("¡Proceso de lectura finalizado!")
+    elif menu == "4":
+        break
+    else:
+        print("Datos incorrectos...")
---
+++ cadenas-2.1.py
@@ -0,0 +1,439 @@
+# Cadenas 2.1 | Jesús Alberto Domínguez Charris
+
+"""
+Secuencial + funciones
+cadena:
+    invertida
+    longitud
+    vector
+    palabras
+
+    vocales SIN REPETIR + TILDES
+    consonantes SIN REPETIR
+    números SIN REPETIR
+    signos de puntuación SIN REPETIR
+    otros caracteres SIN REPETIR
+
+    palabras CANTIDAD
+    vocales CANTIDAD
+    consonantes CANTIDAD
+    números CANTIDAD
+    signos de puntuación CANTIDAD
+    otros caracteres CANTIDAD
+    mayúsculas CANTIDAD
+    minúsculas CANTIDAD
+    espacios CANTIDAD
+
+menú
+guardar en txt
+leer txt
+funciones
+time sleep
+guardar json
+leer json
+guardar morse
+leer morse
+diccionario morse mejorado
+"""
+from time import sleep
+import json
+
+cadena = ""
+invertir = ""
+vector = []
+longitud = 0
+palabras = []
+cantidad_palabras = 0
+cantidad_vocales = 0
+vocales = ["a", "e", "i", "o", "u",
+           "á", "é", "í", "ó", "ú",
+           "à", "è", "ì", "ò", "ù",
+           "ä", "ë", "ï", "ö", "ü"]
+vocales_cadena = []
+cantidad_consonantes = 0
+consonantes = ["b", "c", "d", "f",
+               "g", "h", "j", "k",
+               "l", "m", "n", "ñ",
+               "p", "q", "r", "s",
+               "t", "v", "w", "x",
+               "y", "z"]
+consonantes_cadena = []
+cantidad_numeros = 0
+numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+numeros_cadena = []
+cantidad_puntuacion = 0
+puntuacion = ["¿", "?", "¡", "!",
+              "(", ")", "[", "]",
+              "{", "}", ".", ":",
+              ",", ";"]
+puntuacion_cadena = []
+cantidad_otros = 0
+otros = ["+", "-", "*", "/", "%",
+         "<", ">", "=", "|", "°",
+         "¬", "#", "$", "&", "\\",
+         "¨", "´", "~", "^", "`",
+         "_", "@"]
+otros_cadena = []
+cantidad_mayusculas = 0
+cantidad_minusculas = 0
+cantidad_espacios = 0
+diccionario = {}
+morse = {
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "ñ": "--.--",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.---",
+    "z": "--..",
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    " ": "/",
+    "á": ".--.-",
+    "é": "..-..",
+    "í": "..",
+    "ó": "---.",
+    "ú": "..-",
+    "¿": "..-.-",
+    "?": "..--..",
+    "¡": "--...-",
+    "!": "-.-.--",
+    ".": ".-.-.-",
+    ":": "---...",
+    ",": "--..--",
+    ";": "-.-.-.",
+    "(": "-.--.",
+    ")": "-.--.-",
+    "+": ".-.-.",
+    "-": "-....-",
+    "/": "-..-.",
+    "=": "-...-",
+    "&": ".-...",
+    "@": ".--.-.",
+    "$": "...-..-",
+    "'": ".----.",
+    "_": "..--.-"
+}
+morse_2 = {
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "--.--": "ñ",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.---": "y",
+    "--..": "z",
+    "-----": "0",
+    ".----": "1",
+    "..---": "2",
+    "...--": "3",
+    "....-": "4",
+    ".....": "5",
+    "-....": "6",
+    "--...": "7",
+    "---..": "8",
+    "----.": "9",
+    "/": " ",
+    ".--.-": "á",
+    "..-..": "é",
+    "---.": "ó",
+    "..-.-": "¿",
+    "..--..": "?",
+    "--...-": "¡",
+    "-.-.--": "!",
+    ".-.-.-": ".",
+    "---...": ":",
+    "--..--": ",",
+    "-.-.-.": ";",
+    "-.--.": "(",
+    "-.--.-": ")",
+    ".-.-.": "+",
+    "-....-": "-",
+    "-..-.": "/",
+    "-...-": "=",
+    ".-...": "&",
+    ".--.-.": "@",
+    "...-..-": "$",
+    ".----.": "'",
+    "..--.-": "_"
+}
+
+
+def leer_cadena():
+    global cadena
+    global invertir
+    global vector
+    global longitud
+    global palabras
+    global cantidad_palabras
+    global cantidad_vocales
+    global vocales
+    global vocales_cadena
+    global cantidad_consonantes
+    global consonantes
+    global consonantes_cadena
+    global cantidad_numeros
+    global numeros
+    global numeros_cadena
+    global cantidad_puntuacion
+    global puntuacion
+    global puntuacion_cadena
+    global cantidad_otros
+    global otros
+    global otros_cadena
+    global cantidad_mayusculas
+    global cantidad_minusculas
+    global cantidad_espacios
+
+    cadena = input("Ingrese su cadena: ")
+    # 1. Invertir
+    invertir = cadena[::-1]
+    # 2. Longitud
+    longitud = len(cadena)
+    # 3. Vector caracteres
+    vector = []
+    for x in cadena:
+        vector.append(x)
+    # 4. Lista palabras
+    palabras = cadena.split()
+    cantidad_palabras = len(palabras)
+    # 5. Vocales
+    cantidad_vocales = 0
+    vocales_cadena = []
+    for x in cadena:
+        if x.lower() in vocales:
+            cantidad_vocales += 1
+            if x not in vocales_cadena:
+                vocales_cadena.append(x)
+    # 6. Consonantes
+    cantidad_consonantes = 0
+    consonantes_cadena = []
+    for x in cadena:
+        if x.lower() in consonantes:
+            cantidad_consonantes += 1
+            if x not in consonantes_cadena:
+                consonantes_cadena.append(x)
+    # 7. Números
+    cantidad_numeros = 0
+    numeros_cadena = []
+    for x in cadena:
+        if x in numeros:
+            cantidad_numeros += 1
+            if x not in numeros_cadena:
+                numeros_cadena.append(x)
+    # 8. Signos de puntuación
+    cantidad_puntuacion = 0
+    puntuacion_cadena = []
+    for x in cadena:
+        if x.lower() in puntuacion:
+            cantidad_puntuacion += 1
+            if x not in puntuacion_cadena:
+                puntuacion_cadena.append(x)
+    # 9. Otros caracteres
+    cantidad_otros = 0
+    otros_cadena = []
+    for x in cadena:
+        if x.lower() in otros:
+            cantidad_otros += 1
+            if x not in otros_cadena:
+                otros_cadena.append(x)
+    # 10. Cantidad mayúsculas y minúsculas
+    cantidad_mayusculas = 0
+    cantidad_minusculas = 0
+    for x in cadena:
+        if x.isupper():
+            cantidad_mayusculas += 1
+        if x.islower():
+            cantidad_minusculas += 1
+    # 11. Cantidad espacios
+    cantidad_espacios = 0
+    for x in cadena:
+        if x.isspace():
+            cantidad_espacios += 1
+
+
+# Ciclo menú
+while True:
+    # Menú
+    print("CADENAS 2.1 | Jesús Alberto Domínguez Charris")
+    print("1. Consola\n2. Guardar txt\n3. Leer txt")
+    print("4. Guardar json\n5. Leer json\n6. Guardar morse")
+    print("7. Leer morse\n8. Salir")
+    menu = input(">>> ")
+
+    # Consola
+    if menu == "1":
+        leer_cadena()
+        print(f"Cadena: {cadena}")
+        print(f"1. Invertir: {invertir}")
+        print(f"2. Longitud: {longitud}")
+        print(f"3. Lista caracteres: {vector}")
+        print(f"4. Lista palabras: {palabras}")
+        print(f"5. Lista vocales: {vocales_cadena}")
+        print(f"6. Lista consonantes: {consonantes_cadena}")
+        print(f"7. Lista números: {numeros_cadena}")
+        print(f"8. Lista signos de puntuación: {puntuacion_cadena}")
+        print(f"9. Lista otros caracteres: {otros_cadena}")
+        print(f"10. Cantidad palabras: {cantidad_palabras}")
+        print(f"11. Cantidad vocales: {cantidad_vocales}")
+        print(f"12. Cantidad consonantes: {cantidad_consonantes}")
+        print(f"13. Cantidad números: {cantidad_numeros}")
+        print(f"14. Cantidad signos de puntuación: {cantidad_puntuacion}")
+        print(f"15. Cantidad otros caracteres: {cantidad_otros}")
+        print(f"16. Cantidad mayúsculas: {cantidad_mayusculas}")
+        print(f"17. Cantidad minúsculas: {cantidad_minusculas}")
+        print(f"18. Cantidad espacios: {cantidad_espacios}\n")
+
+    # Guardar txt
+    elif menu == "2":
+        leer_cadena()
+        with open("cadenas-2.1.txt", "w", encoding="utf-8") as file:
+            file.write(f"Cadena: {cadena}\n")
+            file.write(f"1. Invertir: {invertir}\n")
+            file.write(f"2. Longitud: {longitud}\n")
+            file.write(f"3. Lista caracteres: {vector}\n")
+            file.write(f"4. Lista palabras: {palabras}\n")
+            file.write(f"5. Lista vocales: {vocales_cadena}\n")
+            file.write(f"6. Lista consonantes: {consonantes_cadena}\n")
+            file.write(f"7. Lista números: {numeros_cadena}\n")
+            file.write(f"8. Lista signos de puntuación: {puntuacion_cadena}\n")
+            file.write(f"9. Lista otros caracteres: {otros_cadena}\n")
+            file.write(f"10. Cantidad palabras: {cantidad_palabras}\n")
+            file.write(f"11. Cantidad vocales: {cantidad_vocales}\n")
+            file.write(f"12. Cantidad consonantes: {cantidad_consonantes}\n")
+            file.write(f"13. Cantidad números: {cantidad_numeros}\n")
+            file.write(f"14. Cantidad signos de puntuación: {cantidad_puntuacion}\n")
+            file.write(f"15. Cantidad otros caracteres: {cantidad_otros}\n")
+            file.write(f"16. Cantidad mayúsculas: {cantidad_mayusculas}\n")
+            file.write(f"17. Cantidad minúsculas: {cantidad_minusculas}\n")
+            file.write(f"18. Cantidad espacios: {cantidad_espacios}")
+            print("¡Proceso de guardado txt finalizado!\n")
+
+    # Leer txt
+    elif menu == "3":
+        with open("cadenas-2.1.txt", "r", encoding="utf-8") as file:
+            contenido = file.read()
+            print(contenido)
+            print("¡Proceso de lectura txt finalizado!\n")
+
+    # Guardar json
+    elif menu == "4":
+        leer_cadena()
+        diccionario = {
+            "cadena": cadena,
+            "invertir": invertir,
+            "vector": vector,
+            "longitud": longitud,
+            "palabras": palabras,
+            "cantidad_palabras": cantidad_palabras,
+            "cantidad_vocales": cantidad_vocales,
+            "vocales_cadena": vocales_cadena,
+            "cantidad_consonantes": cantidad_consonantes,
+            "consonantes_cadena": consonantes_cadena,
+            "cantidad_numeros": cantidad_numeros,
+            "numeros_cadena": numeros_cadena,
+            "cantidad_puntuacion": cantidad_puntuacion,
+            "puntuacion_cadena": puntuacion_cadena,
+            "cantidad_otros": cantidad_otros,
+            "otros_cadena": otros_cadena,
+            "cantidad_mayusculas": cantidad_mayusculas,
+            "cantidad_minusculas": cantidad_minusculas,
+            "cantidad_espacios": cantidad_espacios
+        }
+        print(type(diccionario), diccionario)
+        print(type(json.dumps(diccionario)), json.dumps(diccionario))
+        with open("json-2.1.txt", "w", encoding="utf-8") as file:
+            json.dump(diccionario, file)
+        print("¡Proceso de guardado json finalizado!\n")
+
+    # Leer json
+    elif menu == "5":
+        with open("json-2.1.txt", "r", encoding="utf-8") as file:
+            json_cadena = file.read()
+            print(type(json_cadena), json_cadena)
+
+            diccionario = json.loads(json_cadena)
+            print(type(diccionario), diccionario)
+            print("¡Proceso de lectura json finalizado!\n")
+
+    # Guardar morse
+    elif menu == "6":
+        leer_cadena()
+        cadena_morse = ""
+        for i in cadena.lower():
+            cadena_morse += " " + morse.get(i)
+        print(cadena_morse)
+        with open("morse-2.1.txt", "w", encoding="utf-8") as file:
+            file.write(cadena_morse)
+        print("¡Proceso de guardado morse finalizado!\n")
+
+    # Leer morse
+    elif menu == "7":
+        with open("morse-2.1.txt", "r", encoding="utf-8") as file:
+            cadena_morse = file.read()
+            lista_morse = cadena_morse.split()
+            cadena = ""
+            for i in lista_morse:
+                cadena += morse_2.get(i)
+            print(cadena_morse)
+            print(cadena)
+            print("¡Proceso de lectura morse finalizado!\n")
+
+    # Salir
+    elif menu == "8":
+        print("¡Hasta luego!")
+        break
+
+    # Datos incorrectos
+    else:
+        print("Datos incorrectos...")
+
+    # Pausa entre cada proceso
+    sleep(1.5)
