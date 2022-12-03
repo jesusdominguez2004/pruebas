22d9b965f83afafffb698b16082e1184cdfd3b0f
jesusdominguez2004 <103864058+jesusdominguez2004@users.noreply.github.com>
2022-12-03 15:02:46 -0500

    Add files via upload

---
+++ saludar.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string nombre;
+    cout << "Ingrese su nombre: "; 
+    cin >> nombre;
+
+    cout << "Hola " << nombre;
+    return 0;
+}
