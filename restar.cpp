da5478455c969cc40db2c23aa4c5d97efda7d7e7
jesusdominguez2004 <103864058+jesusdominguez2004@users.noreply.github.com>
2022-12-04 11:13:03 -0500

    Add files via upload

---
+++ restar.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, resta;
+    cout << "Ingrese num1: "; cin >> num1;
+    cout << "Ingrese num2: "; cin >> num2;
+    resta = num1 - num2;
+    cout << num1 << " - " << num2 << " = " << resta;
+    return 0;
+}
---
+++ sumar.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, suma;
+    cout << "Ingrese num1: "; cin >> num1;
+    cout << "Ingrese num2: "; cin >> num2;
+    suma = num1 + num2;
+    cout << num1 << " + " << num2 << " = " << suma;
+    return 0;
+}
